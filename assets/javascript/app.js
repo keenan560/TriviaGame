cd6268246990a504975a7273f7c8d63ed30a49f5
keenan560 <keenan560@gmail.com>
2019-05-24 10:08:02 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -84,11 +84,11 @@ images[11] = "assets/images/tower.jpg";
 function changeBackground() {
     
     if (i < images.length - 1 ) {
-        $('body').fadeOut("fast", function () {
+        $('body').fadeOut(100, function () {
             $('body').css({
                 'background-image': "url('" + images[i] + "')"
             });
-            $('body').fadeIn("fast");
+            $('body').fadeIn(100);
         }); 
         i++;
     } else {
